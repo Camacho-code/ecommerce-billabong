db68fda2c5b89c97779ea28dce169f1ee4797981
Camacho-code <camacho.juan.ac@gmail.com>
2022-12-14 21:58:22 -0300

    Header update}

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,36 +2,36 @@ import React from "react";
 import Search from "../../assets/icons/Search.svg";
 import User from "../../assets/icons/User.svg";
 import Bag from "../../assets/icons/Bag.svg";
+import Logo from "../../assets/icons/Logo.png";
 import "./index.css";
 
 export default function Header() {
   return (
-    <header className="search-container">
-      <nav>
-        <ul>
-          <li>
-            <a href="">
-              <img src="https://d3ugyf2ht6aenh.cloudfront.net/stores/987/679/themes/common/logo-134625903-1651507122-d9433efbae55018cfe47865a3f64c6801651507122-320-0.png?0"></img>
-            </a>
-          </li>
+    <header>
+      <nav className="container">
+        <div>
+          <a href="">
+            <img src={Logo} height={42}></img>
+          </a>
+        </div>
 
-          <li>
-            <a href="">
-              <img src={Search} height={36}></img>
-            </a>
-          </li>
-          <li>
-            <a>
-              <img src={User} height={52}></img>
-            </a>
-          </li>
+        <div>
+          <a href="">
+            <img src={Search} height={36}></img>
+          </a>
+        </div>
 
-          <li>
-            <a>
-              <img src={Bag} height={36}></img>
-            </a>
-          </li>
-        </ul>
+        <div>
+          <a href="">
+            <img src={User} height={70}></img>
+          </a>
+        </div>
+
+        <div>
+          <a href="">
+            <img src={Bag} height={46}></img>
+          </a>
+        </div>
       </nav>
     </header>
   );
